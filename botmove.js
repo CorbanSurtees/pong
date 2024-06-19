cae0f4d890179b0daea3bab4872ded10dee20214
Corban Surtees <surco973@student.otago.ac.nz>
2024-06-19 22:29:48 +1200

    nerf again

--- botmove.js
+++ botmove.js
@@ -2,10 +2,10 @@
 var hp=1
 function botmove(){
 if (y_1+p1h/hp<by&& y_1 < (height-p1h)) {
-y_1=y_1+2+psi
+y_1=y_1+3+psi
 } else
 if (y_1+p1h/hp>by && y_1 > 0) {
-y_1=y_1-1-psi
+y_1=y_1-3-psi
 }  else
   if (y_1+p1h/hp==by) {
   y_1=y_1
